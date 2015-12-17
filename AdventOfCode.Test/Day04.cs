e52e9bd07e769ad6551b4b9355450894f22a4f08
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2015-12-17 22:32:28 +0100

    Day 4

---
+++ AdventOfCode.Test/Day04.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace AdventOfCode.Test
+{
+    [TestClass]
+    public class Day04
+    {
+        private CoinMiner miner;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            miner = new CoinMiner();
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual("609043", miner.NextHash("abcdef",5));
+            Assert.AreEqual("1048970", miner.NextHash("pqrstuv", 5));
+
+            Trace.WriteLine(string.Format("Next hash starting with 5 0's {0}", miner.NextHash("ckczppom", 5)));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Trace.WriteLine(string.Format("Next hash starting with 6 0's {0}", miner.NextHash("ckczppom", 6)));
+        }
+    }
+}
---
+++ AdventOfCode/CoinMiner.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class CoinMiner
+    {
+        public string NextHash(string secret, int zeros)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                return (from num in Enumerable.Range(0, int.MaxValue)
+                       let hash = md5.ComputeHash(Encoding.UTF8.GetBytes(secret + num))
+                       where hash.Take(zeros/2).All(x => x == 0)
+                           && (zeros % 2 == 0 || hash[zeros/2] < 0x10)
+                       select num).First().ToString();
+            }
+        }
+    }
+}
