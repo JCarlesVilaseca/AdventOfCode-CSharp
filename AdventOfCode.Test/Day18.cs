e21d3fd299e18cb4269e22ea25ec555a3812358a
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2015-12-19 22:59:58 +0100

    Unfinished day 18

---
+++ AdventOfCode.Test/Day18.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Test
+{
+    [TestClass]
+    public class Day18
+    {
+        private List<int> input = new List<int>();
+        private GameOfLife game;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            /*
+            using (var file = new StreamReader("Input17.txt"))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                    input.Add(int.Parse(line));
+            }
+            */
+
+            game = new GameOfLife();
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var world = GameOfLife.World.Parse( 
+              @".#.#.#
+                ...##.
+                #....#
+                ..#...
+                #.#..#
+                ####..");
+
+            var next = game.Next(world);
+            Trace.WriteLine(next.ToString());
+            Assert.AreEqual("", next);
+        }
+
+    }
+}
---
+++ AdventOfCode/GameOfLife.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class GameOfLife
+    {
+        public class World
+        {
+            public bool[] Data { get; set; }
+            public int Width { get; set; }
+
+            public override string ToString()
+            {
+                return string.Join(Environment.NewLine,
+                    Data.Select((x, index) => new { x = x, index = index })
+                    .GroupBy(grp => grp.index / Width)
+                    .Select(x => string.Join("", x.Select(g => g.x ? "#" : "."))));
+            }
+
+            public static World Parse(string str)
+            {
+                return new World {
+                    Width = str.IndexOf(Environment.NewLine),
+                    Data = str.Where(x => x=='#' || x=='.').Select(x => x == '#' ? true : false).ToArray()
+                };
+            }
+        }
+
+        public World Next(World world)
+        {
+            return world;
+        }
+    }
+}
